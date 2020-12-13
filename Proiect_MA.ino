468d9b74b55c0ef61f09a2b5f0e1e0b36ba1b1fe
Raymi22 <58049134+Raymi22@users.noreply.github.com>
2020-12-14 01:39:17 +0200

    Code ordering
    Grouped most part of the code in functions. Almost ready, work still left to do at the light measurement part

--- Proiect_MA.ino
+++ Proiect_MA.ino
@@ -3,18 +3,28 @@ const int LED2 = D4;
 const int LED3 = D3;
 const int LED4 = D2;
 const int LED5 = D1;
-const int MOTOR = D0;
-const int buttonHUM=D6;
-const int buttonLIGHT=D7;
-
+const int MOTOR = D0;   //switch for pump
+const int buttonHUM=D6; //button for soil moisture sensor display
+const int buttonLIGHT=D7;  //button for photoresistor display
+const int sensorSELECT=D8;  //sensor selector (0-photoresistor, 1-soil moisture)
 const int sense_Pin = A0; // sensor input at Analog pin A0
 
-int value = 0;
-int buttonHUM_value = 0;
-int s = 0;
-int sm = 0;
 
-void LED_PRINT_LEVEL(int level) //levels shows number of divisions to turn on
+const int S=5; // duration of watering
+const int SM=10; // time interval between waterings
+const int TIME_UNIT=2000; //time unit of one cicle (ms)
+const int SENSOR_READ_TIME=1000;
+
+
+int light=0, moisture=0;
+int buttonHUM_value = 0;
+int s = 0;  //duration of watering index
+int sm = 0; //time interval between waterings index
+
+
+////////////////////////////  LED_PRINT_LEVEL  /////////////////////////////////////
+
+void LED_PRINT_LEVEL(int level) //turns on number of LED dividions according to "level"
 {
   switch(level)
   {
@@ -55,9 +65,50 @@ void LED_PRINT_LEVEL(int level) //levels shows number of divisions to turn on
     
 }
 
+
+////////////////////////////////  READ_SENSOR  ////////////////////////////////////  
+
+void READ_SENSORS()       // contains 2 x SENSOR_READ_TIME delay
+{
+   digitalWrite(sensorSELECT, HIGH);
+   delay(SENSOR_READ_TIME);
+   Serial.print("MOISTURE VALUE: ");
+   moisture = analogRead(sense_Pin);
+   Serial.println(moisture);
+   
+   digitalWrite(sensorSELECT, LOW);
+   delay(SENSOR_READ_TIME);
+   Serial.print("LIGHT VALUE: ");
+   light = analogRead(sense_Pin);
+   Serial.println(light);
+}
+
+
+////////////////////////// GET_MOISTURE_LEVEL ////////////////////////////////////
+
+int GET_MOISTURE_LEVEL(){
+  if(moisture>800) return 1;
+  if(moisture>600) return 2;
+  if(moisture>400) return 3;
+  if(moisture>200) return 4;
+  return 5;
+}
+
+
+////////////////////////// GET_LIGHT_LEVEL ////////////////////////////////////
+
+int GET_LIGHT_LEVEL(){
+  return 0;
+}
+
+
+//////////////////////////////// SETUP /////////////////////////////////////////
+
 void setup() {
    Serial.begin(115200);
    Serial.println("The board has started");
+
+   ////////CONFIGURAION OF PINS////////
    pinMode(LED1, OUTPUT);
    pinMode(LED2, OUTPUT);
    pinMode(LED3, OUTPUT);
@@ -66,54 +117,45 @@ void setup() {
    pinMode(MOTOR, OUTPUT);
    pinMode(buttonHUM, INPUT_PULLUP);
    pinMode(buttonLIGHT, INPUT_PULLUP);
+   pinMode(sensorSELECT, OUTPUT);
+
    delay(2000);
 }
 
+
+
+///////////////////////// MAIN  ////////////////////////////////////
 void loop() {
-   Serial.print("MOISTURE LEVEL : ");
-   value= analogRead(sense_Pin);
-   buttonHUM_value= digitalRead(buttonHUM);
-   Serial.println(value);
    digitalWrite(LED1, LOW);
    digitalWrite(LED2, LOW);
    digitalWrite(LED3, LOW);
    digitalWrite(LED4, LOW);
    digitalWrite(LED5, LOW);
-   if(value>950)
-        {
-            if (buttonHUM_value==0) LED_PRINT_LEVEL(1);
-            if (sm>10){
-              digitalWrite(MOTOR,HIGH);
-              s=1;
-              sm=0;
-            }
-            else sm++;
+   digitalWrite(sensorSELECT, LOW);
+
+   int moisture_level=GET_MOISTURE_LEVEL(moisture);
+   
+   buttonHUM_value = digitalRead(buttonHUM);
+   if (buttonHUM_value==0) LED_PRINT_LEVEL(moisture_level);  // show moisture level on leds if button is pressed
+   
+   if(moisture_level==1){   //need watering
+      if (sm>SM){                 //turns on the pump if a certain time has passed since last watering
+        digitalWrite(MOTOR,HIGH);
+        s=1;
+        sm=0;                
         }
-    else if (value>900)
-        {
-            if (buttonHUM_value==0) LED_PRINT_LEVEL(2);
-        }
-    else if (value>850)
-        {
-            if (buttonHUM_value==0) LED_PRINT_LEVEL(3);
-        }
-    else if (value>800)
-        {
-            if (buttonHUM_value==0) LED_PRINT_LEVEL(4);
-        }
-    else
-        {
-            if (buttonHUM_value==0) LED_PRINT_LEVEL(5);
-        }
+      else sm++;
+    }
         
-    if (s>0)
+    if (s>0) 
     {
-       if(s>5){
+       if(s>S){
         digitalWrite(MOTOR,LOW);
         s=0;
         sm=0;
        }
        else s++;
     }
-    delay(1000);
+    READ_SENSORS();
+    delay(TIME_UNIT-2*SENSOR_READ_TIME);
 }                               
